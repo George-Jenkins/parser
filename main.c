98290190757e3acacc6c089bcbe465fa2ce961d8
George Jenkins <georgejenkins@Georges-MacBook-Pro.local>
2019-04-09 18:11:58 -0400

    Adding global variables

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <ctype.h>
 
-* Global declarations */
+/* Global declarations */
 /* Variables */
 int charClass;
 char lexeme [100];
 char nextChar;
+char retLex[200];
+char tokenStr[100];
+int errorFound = 0;
+int lineCount = 1;
+int lexLen;
 
 int main(int argc, const char * argv[]) {
     
