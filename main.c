5f8585351fd3a23ec9f7db1ead62ae5766f0864a
George Jenkins <georgejenkins@Georges-MacBook-Pro.local>
2019-04-09 18:09:19 -0400

    Adding global variables

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <ctype.h>
 
+* Global declarations */
+/* Variables */
+int charClass;
+char lexeme [100];
+char nextChar;
 
 int main(int argc, const char * argv[]) {
     
